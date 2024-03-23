061b1d9e9c11022b54ab5e760acb4e580766766d
SonikaIngale <ingalesonika@outlook.com>
2024-03-23 20:32:41 +0530

    my file

---
+++ SimpleJavaProgram.java
@@ -0,0 +1,11 @@
+class  SimpleJavaProgram
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Program Start");
+		int num1=25;
+		boolen Status=true;
+		char grade='A';
+		System.out.println("Program ends");
+		}
+}
